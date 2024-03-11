9a980f8e1eb9088fd606101a897b8a9963348576
ursalesangelarose27 <145885184+ursalesangelarose27@users.noreply.github.com>
2024-03-11 15:13:37 +0800

    update admin & customer tab

---
+++ customer/editform.js
@@ -0,0 +1,9 @@
+function showEditForm() {
+    document.getElementById("delivery-address").style.display = "none";
+    document.getElementById("edit-address").style.display = "block";
+}
+
+function hideEditForm() {
+    document.getElementById("edit-address").style.display = "none";
+    document.getElementById("delivery-address").style.display = "block";
+}
