639ef30edb9023f9b7cee388eb8e29de11a59957
Jonnel Lique <jonnelmlique57@gmail.com>
2024-03-26 17:44:39 +0800

    add update design 1.0

--- oldfile/customer1/editform.js
+++
@@ -1,9 +0,0 @@
-function showEditForm() {
-    document.getElementById("delivery-address").style.display = "none";
-    document.getElementById("edit-address").style.display = "block";
-}
-
-function hideEditForm() {
-    document.getElementById("edit-address").style.display = "none";
-    document.getElementById("delivery-address").style.display = "block";
-}
--- public/js/index/footerindex.js
+++
@@ -1,6 +0,0 @@
-fetch('/public/Shared/Layout/footerindex.html')
-.then(response => response.text())
-.then(data => {
-    document.querySelector('body').insertAdjacentHTML('beforeend', data);
-})
-.catch(error => console.error('Error fetching footer:', error));
